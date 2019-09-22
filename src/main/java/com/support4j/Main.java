cb0073142f1556d43fb7c308d5324182b0021f88
support4j <work@support4j.com>
2019-09-23 01:17:29 +0800

    Hello World!

---
+++ src/main/java/com/support4j/Main.java
@@ -0,0 +1,12 @@
+package com.support4j;
+
+/**
+ * @Author work@support4j.com
+ * @Date 2019/9/23 01:14
+ * @Version 0.0.1
+ */
+public class Main {
+    public String toString() {
+        return "Hello World!";
+    }
+}
